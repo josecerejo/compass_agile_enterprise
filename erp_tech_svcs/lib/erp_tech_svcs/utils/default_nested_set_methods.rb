8271a154a2adfad1436a8f02d99f146efc24be2a
Russell Holmes <russellfholmes@gmail.com>
2012-06-06 09:52:02 -0400

    added find_by_ancestor_iids to default_nested_set_methods

--- erp_tech_svcs/lib/erp_tech_svcs/utils/default_nested_set_methods.rb
+++ erp_tech_svcs/lib/erp_tech_svcs/utils/default_nested_set_methods.rb
@@ -38,9 +38,30 @@ module ErpTechSvcs
 				def find_children(parent_id = nil)
 					parent_id.to_i == 0 ? self.roots : find(parent_id).children
 				end
+				
+				# find_by_ancestor_iids
+        # allows you to find a nested set element by the internal_identifiers in its ancestry
+        # for example, to find a GlAccount whose internal_identifier is “site_4”, and whose parent’s internal_identifier is “nightly_room_charge”
+        # and whose grandparent’s internal_identifier is “charge”, you would make this call:
+        # gl_account = GlAccount.find_by_iids(['charge', 'nightly_room_charge', "site_4"])
+        def find_by_ancestor_iids(iids)
+          return nil unless iids.is_a? Array
+
+          node = nil
+          iids.each do |iid|
+            if (iid == iids.first)
+              node = where("parent_id is null and internal_identifier = ?",iid).first
+            else
+              node = where("parent_id = ? and internal_identifier = ?",node.id,iid).first
+            end
+            return nil if node.nil?
+          end
+          return node
+        end
+        
 			end
 
-		end
-	end
-end
+		end #DefaultNestedSetMethods
+	end #Utils
+end #ErpTechSvcs
 
