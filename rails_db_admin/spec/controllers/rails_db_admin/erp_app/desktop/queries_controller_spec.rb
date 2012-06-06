127685eaefef6e28ca70e1ff5bc2748db811a257
Chris Brodt <cbrodt@tnsolutionsinc.com>
2012-06-06 15:44:01 -0400

    fixed controller tests to set action correctly

--- rails_db_admin/spec/controllers/rails_db_admin/erp_app/desktop/queries_controller_spec.rb
+++ rails_db_admin/spec/controllers/rails_db_admin/erp_app/desktop/queries_controller_spec.rb
@@ -15,8 +15,9 @@ describe RailsDbAdmin::ErpApp::Desktop::QueriesController do
 
     it "returns unsuccessful because of an empty result set" do
 
-      post :queries, {:use_route => :rails_db_admin, :action => "execute_query",
-                      :cursor_pos => "0",:sql => "SELECT * FROM relationship_types;"}
+      post :execute_query, {:use_route => :rails_db_admin,
+                            :cursor_pos => "0",
+                            :sql => "SELECT * FROM relationship_types;"}
 
       parsed_body = JSON.parse(response.body)
       parsed_body["success"].should eq(false)
@@ -25,12 +26,11 @@ describe RailsDbAdmin::ErpApp::Desktop::QueriesController do
 
     it "should not throw exception if there is 1 statement and no semi-colon" do
 
-      post :queries, {:use_route => :rails_db_admin,
-                      :action => "execute_query",
-                      :cursor_pos => "0",
-                      :sql => "SELECT * FROM role_types "\
-                              "WHERE internal_identifier"\
-                              " = 'execute_query_test_role'"}
+      post :execute_query, {:use_route => :rails_db_admin,
+                            :cursor_pos => "0",
+                            :sql => "SELECT * FROM role_types "\
+                                    "WHERE internal_identifier"\
+                                    " = 'execute_query_test_role'"}
 
       parsed_body = JSON.parse(response.body)
       parsed_body["success"].should eq(true)
@@ -38,7 +38,7 @@ describe RailsDbAdmin::ErpApp::Desktop::QueriesController do
 
     it "should work on multi-line queries" do
 
-      post :queries, {:use_route => :rails_db_admin, :action => "execute_query",
+      post :execute_query, {:use_route => :rails_db_admin,
                       :cursor_pos => "1",
                       :sql => "SELECT * FROM role_types \n"\
                               "WHERE internal_identifier ="\
@@ -59,7 +59,7 @@ describe RailsDbAdmin::ErpApp::Desktop::QueriesController do
       @sql = CGI::unescape(@sql)
       @cursor_pos = "6"
 
-      post :queries, {:use_route => :rails_db_admin, :action => "execute_query",
+      post :execute_query, {:use_route => :rails_db_admin,
                       :cursor_pos => @cursor_pos,
                       :sql => @sql}
 
@@ -69,7 +69,7 @@ describe RailsDbAdmin::ErpApp::Desktop::QueriesController do
 
     it "should work on input of multiple queries spanning several lines each " do
 
-      post :queries, {:use_route => :rails_db_admin, :action => "execute_query",
+      post :execute_query, {:use_route => :rails_db_admin,
                       :cursor_pos => "1",
                       :sql => "SELECT * FROM role_types \n"\
                               "WHERE internal_identifier = 'execute_query_test_role';\n"\
@@ -100,9 +100,8 @@ describe RailsDbAdmin::ErpApp::Desktop::QueriesController do
       @query_support.should_receive(:execute_sql).and_return([columns, values, nil])
       RailsDbAdmin::QuerySupport.should_receive(:new).and_return(@query_support)
 
-      post :queries, {:use_route => :rails_db_admin,
-                      :action => "open_and_execute_query",
-                      :query_name => "some_query"}
+      post :open_and_execute_query, {:use_route => :rails_db_admin,
+                                     :query_name => "some_query"}
 
       parsed_body = JSON.parse(response.body)
       parsed_body["success"].should eq(true)
@@ -114,9 +113,8 @@ describe RailsDbAdmin::ErpApp::Desktop::QueriesController do
 
     it "should return true, a sql statement, grid columns & fields, and data" do
 
-      post :queries, {:use_route => :rails_db_admin,
-                      :action => "select_top_fifty",
-                      :table => "role_types"}
+      post :select_top_fifty, {:use_route => :rails_db_admin,
+                               :table => "role_types"}
 
       expected_sql = "SELECT  * FROM \"role_types\"  LIMIT 50"
 
