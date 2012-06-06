3cd0e2b0f673238687d23dbc9f609590ea9640fd
Adam H <adam@ol7896.olcc.lan>
2012-06-06 14:44:32 -0400

    merging s3 fixes from rails32 branch into orangelake (95bea11b3785c3221dc073e26d13ac2b15a1a125) erp_tech_svcs bugfixes and specs pass
    Conflicts:
    	erp_tech_svcs/lib/erp_tech_svcs/file_support/s3_manager.rb

--- erp_tech_svcs/spec/spec_helper.rb
+++ erp_tech_svcs/spec/spec_helper.rb
@@ -39,15 +39,15 @@ end
 
 Spork.each_run do
   #We have to execute the migrations from dummy app directory
-  #Dir.chdir DUMMY_APP_ROOT
-  #`rake db:drop`
-  #Dir.chdir ENGINE_RAILS_ROOT
+  Dir.chdir DUMMY_APP_ROOT
+  `rake db:drop`
+  Dir.chdir ENGINE_RAILS_ROOT
 
   #We have to execute the migrations from dummy app directory
-  #Dir.chdir DUMMY_APP_ROOT
-  #`rake db:migrate`
-  #`rake db:migrate_data`
-  #Dir.chdir ENGINE_RAILS_ROOT
+  Dir.chdir DUMMY_APP_ROOT
+  `rake db:migrate`
+  `rake db:migrate_data`
+  Dir.chdir ENGINE_RAILS_ROOT
 
   ErpDevSvcs::FactorySupport.load_engine_factories
 
