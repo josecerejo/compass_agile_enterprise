b36a06ff1f1774246c8875c4980ac4f038b51a8b
Adam H <adam@ol9667.olcc.lan>
2012-06-14 17:02:48 -0400

    fixed insert link at cursor

--- knitkit/public/javascripts/erp_app/desktop/applications/knitkit/file_assets_panel.js
+++ knitkit/public/javascripts/erp_app/desktop/applications/knitkit/file_assets_panel.js
@@ -171,7 +171,7 @@ Compass.ErpApp.Desktop.Applications.Knitkit.FileAssetsPanel = function(module) {
     },
     {
       nodeType:'leaf',
-      text:'Insert link at cursor (inline)',
+      text:'Insert link at cursor',
       iconCls:'icon-add',
       listeners:{
         scope:self,
@@ -180,6 +180,22 @@ Compass.ErpApp.Desktop.Applications.Knitkit.FileAssetsPanel = function(module) {
           Ext.MessageBox.prompt('Display Name', 'Please enter display name:', function(btn, text){
             if(btn == 'ok'){
               self.module.centerRegion.insertHtmlIntoActiveCkEditor('<a href="/download/'+node.data.text+'?path='+node.data.downloadPath+'">'+text+'</a>');
+            }
+          });
+        }
+      }      
+    },
+    {
+      nodeType:'leaf',
+      text:'Insert link at cursor (inline)',
+      iconCls:'icon-add',
+      listeners:{
+        scope:self,
+        'click':function(){
+          var node = this.websiteFileAssetsTreePanel.selectedNode;
+          Ext.MessageBox.prompt('Display Name', 'Please enter display name:', function(btn, text){
+            if(btn == 'ok'){
+              self.module.centerRegion.insertHtmlIntoActiveCkEditor('<a href="/download/'+node.data.text+'?path='+node.data.downloadPath+'&disposition=inline">'+text+'</a>');
             }
           });
         }
--- knitkit/public/javascripts/erp_app/desktop/applications/knitkit/image_assets_panel.js
+++ knitkit/public/javascripts/erp_app/desktop/applications/knitkit/image_assets_panel.js
@@ -153,7 +153,7 @@ Compass.ErpApp.Desktop.Applications.Knitkit.ImageAssetsPanel = function(module) 
     },
     {
       nodeType:'leaf',
-      text:'Insert link at cursor (prompt)',
+      text:'Insert link at cursor',
       iconCls:'icon-add',
       listeners:{
         scope:self,
@@ -162,6 +162,22 @@ Compass.ErpApp.Desktop.Applications.Knitkit.ImageAssetsPanel = function(module) 
           Ext.MessageBox.prompt('Display Name', 'Please enter display name:', function(btn, text){
             if(btn == 'ok'){
               self.module.centerRegion.insertHtmlIntoActiveCkEditor('<a href="/download/'+node.data.text+'?path='+node.data.downloadPath+'">'+text+'</a>');
+            }
+          });
+        }
+      }
+    },
+    {
+      nodeType:'leaf',
+      text:'Insert link at cursor (inline)',
+      iconCls:'icon-add',
+      listeners:{
+        scope:self,
+        'click':function(){
+          var node = this.websiteImageAssetsTreePanel.selectedNode;
+          Ext.MessageBox.prompt('Display Name', 'Please enter display name:', function(btn, text){
+            if(btn == 'ok'){
+              self.module.centerRegion.insertHtmlIntoActiveCkEditor('<a href="/download/'+node.data.text+'?path='+node.data.downloadPath+'&disposition=inline">'+text+'</a>');
             }
           });
         }
