56eaef87129db60dfb8c4ab349cd0ba6d7476ebf
Greg Pederson <greg@nsightdevelopment.com>
2012-06-29 11:48:01 -0400

    troubleshooting commit

--- knitkit/app/models/website.rb
+++ knitkit/app/models/website.rb
@@ -319,7 +319,7 @@ class Website < ActiveRecord::Base
           :email_inquiries => setup_hash[:email_inquiries],
           :auto_activate_publication => setup_hash[:auto_activate_publication]
         )
-        website.errors.full_messages
+        Rails.logger.error { "#{website.errors.full_messages}" }
         website.reload #reloading this object to ensure published_websites are loaded
         Rails.logger.error { "*********************************************" }
         Rails.logger.error { "*********************************************" }
