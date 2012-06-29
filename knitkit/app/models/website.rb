1fdc54a799f6ac98c523ca990dfe6ae9696a40bc
Adam H <adam@ol7345.olcc.lan>
2012-06-29 10:55:57 -0400

    debug logging

--- knitkit/app/models/website.rb
+++ knitkit/app/models/website.rb
@@ -98,7 +98,8 @@ class Website < ActiveRecord::Base
   end
 
   def setup_website
-    PublishedWebsite.create(:website => self, :version => 0, :active => true, :comment => 'New Site Created')
+    pw = PublishedWebsite.create(:website => self, :version => 0, :active => true, :comment => 'New Site Created')
+    Rails.logger info "@@@@@@@@ PublishedWebsite: #{pw.inspect}"
     Role.create(:description => "Website #{self.title}", :internal_identifier => website_role_iid) if self.role.nil?
     configuration = ::Configuration.find_template('default_website_configuration').clone(true)
     configuration.description = "Website #{self.name} Configuration"
