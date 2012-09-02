c36a342c772c9750cf9de99d42ede3cf0b76d56c
Adam H <adam@adam.local>
2012-09-02 16:14:22 -0400

    adding route for comments `

--- erp_forms/config/routes.rb
+++ erp_forms/config/routes.rb
@@ -3,4 +3,5 @@ ErpForms::Engine.routes.draw do
   match '/erp_app/desktop/dynamic_forms/data/:action(/:model_name)' => 'erp_app/desktop/dynamic_forms/data'
   match '/erp_app/desktop/dynamic_forms/forms/:action' => 'erp_app/desktop/dynamic_forms/forms'
   match '/erp_app/desktop/dynamic_forms/models/:action' => 'erp_app/desktop/dynamic_forms/models'
+  match '/erp_app/desktop/dynamic_forms/comments/:action' => 'erp_app/desktop/dynamic_forms/comments'
 end
