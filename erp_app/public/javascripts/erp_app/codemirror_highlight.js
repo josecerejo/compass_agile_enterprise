06c73404e3102bfe4bc3e109355eb7dd371c1db0
Adam H <adam@adam.local>
2012-09-10 20:19:42 -0400

    null check

--- erp_app/public/javascripts/erp_app/codemirror_highlight.js
+++ erp_app/public/javascripts/erp_app/codemirror_highlight.js
@@ -2,13 +2,16 @@ function codemirrorHighlight(target_id) {
   modes = CodeMirror.listModes();
   for (var x = 0; x < modes.length; x++){
     if (modes[x] != 'null'){
-      tags = document.getElementById(target_id).getElementsByTagName(modes[x]);
-      for (var i = 0; i < tags.length; i++) {
-        output = tags[i];
-        output.className += ' cm-s-default';
-        output.style.cssText = "white-space: pre;";
-        CodeMirror.runMode(tags[i].innerHTML, modes[x],  output);
-      }            
+      target_div = document.getElementById(target_id);
+      if (target_div != null){
+        tags = target_div.getElementsByTagName(modes[x]);
+        for (var i = 0; i < tags.length; i++) {
+          output = tags[i];
+          output.className += ' cm-s-default';
+          output.style.cssText = "white-space: pre;";
+          CodeMirror.runMode(tags[i].innerHTML, modes[x],  output);
+        }        
+      }
     }
   }
 }
