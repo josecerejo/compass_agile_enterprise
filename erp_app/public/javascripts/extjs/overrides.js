acd3dfdd988d37d92b05933175f6087490be6a3d
Adam H <adam@adam.olcc.lan>
2012-10-05 09:35:55 -0400

    upgrading ExtJS to 4.1.2a

--- erp_app/public/javascripts/extjs/overrides.js
+++ erp_app/public/javascripts/extjs/overrides.js
@@ -24,38 +24,36 @@ Ext.override(Ext.ZIndexManager, {
   }
 });
 
-Ext.override(Ext.data.TreeStore, {
-  load: function(options) {
-    options = options || {};
-    options.params = options.params || {};
+// Ext.override(Ext.data.TreeStore, {
+//   load: function(options) {
+//     options = options || {};
+//     options.params = options.params || {};
 
-    var me = this,
-    node = options.node || me.tree.getRootNode(),
-    root;
+//     var me = this,
+//     node = options.node || me.tree.getRootNode(),
+//     root;
 
-    // If there is not a node it means the user hasnt defined a rootnode yet. In this case lets just
-    // create one for them.
-    if (!node) {
-      node = me.setRootNode({
-        expanded: true
-      });
-    }
+//     // If there is not a node it means the user hasnt defined a rootnode yet. In this case lets just
+//     // create one for them.
+//     if (!node) {
+//       node = me.setRootNode({
+//         expanded: true
+//       });
+//     }
 
-    if (me.clearOnLoad) {
-      node.removeAll(false);
-    }
+//     if (me.clearOnLoad) {
+//       node.removeAll(false);
+//     }
 
-    Ext.applyIf(options, {
-      node: node
-    });
-    options.params[me.nodeParam] = node ? node.getId() : 'root';
+//     Ext.applyIf(options, {
+//       node: node
+//     });
+//     options.params[me.nodeParam] = node ? node.getId() : 'root';
 
-    if (node) {
-      node.set('loading', true);
-    }
+//     if (node) {
+//       node.set('loading', true);
+//     }
 
-    return me.callParent([options]);
-  }
-});
-
-
+//     return me.callParent([options]);
+//   }
+// });
