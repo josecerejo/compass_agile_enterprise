34b6adce8e967e5a91d33e98abce7e4ee7559e9e
Adam H <adam@adam.local>
2012-10-17 19:58:24 -0400

    refactored sorted_dynamic_attributes(options={})   # options = {:with_prefix => false, :use_label => false, :all => false}   # :with_prefix = false will remove the dyn_ from the attribute key   # if :with_prefix is false, you may choose to use the fieldLabel as the hash key by setting :use_label = true   # :use_label is useful displaying data on a view screen or formatting an email   # :all = false will only return attributes that are in the form definition   # :all = true will return all attributes with those not in the form definition last   # you can set :all = true and :use_label = true, but attributes not in definition will use key.titleize   # if for some reason a form cannot be found, sorting will not be attempted

--- erp_forms/app/models/dynamic_datum.rb
+++ erp_forms/app/models/dynamic_datum.rb
@@ -35,41 +35,67 @@ class DynamicDatum < ActiveRecord::Base
   # we cannot assume that dynamic attributes are stored in order in the database as this is often not the case
   # this method will sort them according to the order of the fields in the form definition
   # method returns an ordered hash
-  # if with_prefix is false, you may choose to use the fieldLabel as the hash key, this is useful displaying data on a view screen
-  def sorted_dynamic_attributes(with_prefix=false, use_label=false)    
+  # options = {:with_prefix => false, :use_label => false, :all => false}
+  # :with_prefix = false will remove the dyn_ from the attribute key
+  # if :with_prefix is false, you may choose to use the fieldLabel as the hash key by setting :use_label = true 
+  # :use_label is useful displaying data on a view screen or formatting an email
+  # :all = false will only return attributes that are in the form definition
+  # :all = true will return all attributes with those not in the form definition last
+  # you can set :all = true and :use_label = true, but attributes not in definition will use key.titleize
+  # if for some reason a form cannot be found, sorting will not be attempted
+  def sorted_dynamic_attributes(options={})
+    options[:with_prefix] = false if options[:with_prefix].nil?
+    options[:use_label] = false if options[:use_label].nil?
+    options[:all] = true if options[:all].nil?
+
     form = self.updated_with_form if form.nil? and !self.updated_with_form.nil?
     form = self.created_with_form if form.nil? and !self.created_with_form.nil?
     form = DynamicForm.get_form(self.reference_type) if form.nil?
     
     unless form.nil?
-      if with_prefix
+      if options[:with_prefix]
         keys = form.definition_object.collect{|f| DYNAMIC_ATTRIBUTE_PREFIX + f[:name]}
       else        
-        labels = form.definition_object.collect{|f| f[:fieldLabel]} if use_label
+        labels = form.definition_object.collect{|f| f[:fieldLabel]} if options[:use_label]
         keys = form.definition_object.collect{|f| f[:name]}
       end
 
       sorted = {}
       i=0
       keys.each do |key|
-        if with_prefix
+        if options[:with_prefix]
           sorted[key] = self.dynamic_attributes[key]
         else
-          index = (use_label ? labels[i] : key)
+          index = (options[:use_label] ? labels[i] : key)
           sorted[index] = self.dynamic_attributes_without_prefix[key]
         end        
 
         i += 1
       end
       
+      if options[:all]
+        # append attributes not in definition
+        attrs = (options[:with_prefix] ? self.dynamic_attributes : self.dynamic_attributes_without_prefix)
+        sorted.each do |k,v|
+          attrs.delete(k)
+        end
+        attrs.each do |k,v|
+          if options[:with_prefix]
+            sorted[k] = self.dynamic_attributes[k]
+          else
+            index = (options[:use_label] ? k.titleize : k)
+            sorted[index] = self.dynamic_attributes_without_prefix[k]
+          end
+        end
+      end
+
       return sorted
     else
-      if with_prefix
+      if options[:with_prefix]
         return self.dynamic_attributes
       else
         return self.dynamic_attributes_without_prefix
       end
     end
   end
-  
 end
