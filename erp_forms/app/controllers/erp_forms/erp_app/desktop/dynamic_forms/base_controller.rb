539bbbec392b4036deb9ded01042f5c025643929
Adam H <adam@adam.att.net>
2012-10-22 15:38:12 -0400

    adding file as ignored param

--- erp_forms/app/controllers/erp_forms/erp_app/desktop/dynamic_forms/base_controller.rb
+++ erp_forms/app/controllers/erp_forms/erp_app/desktop/dynamic_forms/base_controller.rb
@@ -1,6 +1,6 @@
 class ErpForms::ErpApp::Desktop::DynamicForms::BaseController < ::ErpApp::Desktop::BaseController
   @@datetime_format = "%m/%d/%Y %l:%M%P"
-  IGNORED_PARAMS = %w{action controller uuid widget_name widget_action dynamic_form_id dynamic_form_model_id model_name use_dynamic_form authenticity_token}
+  IGNORED_PARAMS = %w{action controller uuid widget_name widget_action dynamic_form_id dynamic_form_model_id model_name use_dynamic_form authenticity_token file}
 
   protected  
   def page
