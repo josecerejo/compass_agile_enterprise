39f70c9182cb9a341e2f4a402cade3aa4344f142
Adam H <adam@ol7335.olcc.lan>
2012-10-22 11:48:05 -0400

    added Ext.onReady

--- erp_app/public/javascripts/extjs/vtypes.js
+++ erp_app/public/javascripts/extjs/vtypes.js
@@ -1,18 +1,20 @@
-Ext.apply(Ext.form.field.VTypes, {
-    //  vtype validation function
-    file : function(value, field) {
-        var file_size = ErpTechSvcs.Config.max_file_size_in_mb * 1024 * 1024;
-        var file_types = ErpTechSvcs.Config.file_upload_types.replace(/\s/g,'|').replace(/,/g,'|');
-        //var fileName = new RegExp('^.*\.('+file_types+')$','i');
-        var fileName = eval('/^.*\.('+file_types+')$/i');
-        // console.log(file_size);
-        // console.log(field.inputEl.dom.files[0].size);
-        // if (field.inputEl.dom.files[0].size > file_size)) return false;
-        return fileName.test(value);
-    },
-    // vtype Text property to display error Text
-    // when the validation function returns false
-    fileText : "File must be no larger than "+ErpTechSvcs.Config.max_file_size_in_mb+"MB and one of the following file types: "+ErpTechSvcs.Config.file_upload_types,
-    // vtype Mask property for keystroke filter mask
-    fileMask : /[a-z_\.]/i
+Ext.onReady(function(){
+    Ext.apply(Ext.form.field.VTypes, {
+        //  vtype validation function
+        file : function(value, field) {
+            var file_size = ErpTechSvcs.Config.max_file_size_in_mb * 1024 * 1024;
+            var file_types = ErpTechSvcs.Config.file_upload_types.replace(/\s/g,'|').replace(/,/g,'|');
+            //var fileName = new RegExp('^.*\.('+file_types+')$','i');
+            var fileName = eval('/^.*\.('+file_types+')$/i');
+            // console.log(file_size);
+            // console.log(field.inputEl.dom.files[0].size);
+            // if (field.inputEl.dom.files[0].size > file_size)) return false;
+            return fileName.test(value);
+        },
+        // vtype Text property to display error Text
+        // when the validation function returns false
+        fileText : "File must be no larger than "+ErpTechSvcs.Config.max_file_size_in_mb+"MB and one of the following file types: "+ErpTechSvcs.Config.file_upload_types,
+        // vtype Mask property for keystroke filter mask
+        fileMask : /[a-z_\.]/i
+    });
 });
