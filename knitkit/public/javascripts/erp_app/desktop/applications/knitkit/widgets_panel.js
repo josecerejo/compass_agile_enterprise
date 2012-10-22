cd870b115db6089ebbef85c30b8103e589ea4cc4
Adam H <adam@ol7335.olcc.lan>
2012-10-22 11:52:05 -0400

    fix for extJS rendering bug. tree tried to load faster than extjs can render tab converted east region to ExtJS 4 code

--- knitkit/public/javascripts/erp_app/desktop/applications/knitkit/widgets_panel.js
+++ knitkit/public/javascripts/erp_app/desktop/applications/knitkit/widgets_panel.js
@@ -1,56 +1,55 @@
-Compass.ErpApp.Desktop.Applications.Knitkit.WidgetsPanel = function() {
-  var widgetsStore = Ext.create('Ext.data.Store',{
-    autoDestroy: true,
-    fields:['name', 'iconUrl', 'onClick', 'about'],
-    data:Compass.ErpApp.Widgets.AvailableWidgets
-  });
+Ext.define("Compass.ErpApp.Desktop.Applications.Knitkit.WidgetsPanel",{
+  extend:"Ext.panel.Panel",
+  alias:'widget.knitkit_WidgetsPanel',
 
-  this.widgetsDataView = Ext.create("Ext.view.View",{
-    style:'overflow:auto',
-    itemSelector: 'div.thumb-wrap',
-    store:widgetsStore,
-    tpl: [
-    '<tpl for=".">',
-    '<div data-qtip="{about}" class="thumb-wrap" id="{name}">',
-    '<div class="thumb"><img src="{iconUrl}" class="thumb-img"></div>',
-    '<span>{name}</span></div>',
-    '</tpl>',
-    '<div class="x-clear"></div>'
-    ],
-    listeners:{
-      'itemcontextmenu':function(view, record, htmlitem, index, e, options){
-        e.stopEvent();
-        var contextMenu = Ext.create("Ext.menu.Menu",{
-          items:[{
-            text:'Add Widget',
-            iconCls:'icon-add',
-            handler:function(btn){
-              record.data.onClick();
-            }
-          }]
-          });
-        contextMenu.showAt(e.xy);
+  constructor : function(config) {
+    var widgetsStore = Ext.create('Ext.data.Store',{
+      autoDestroy: true,
+      fields:['name', 'iconUrl', 'onClick', 'about'],
+      data:Compass.ErpApp.Widgets.AvailableWidgets
+    });
+
+    this.widgetsDataView = Ext.create("Ext.view.View",{
+      style:'overflow:auto',
+      itemSelector: 'div.thumb-wrap',
+      store:widgetsStore,
+      tpl: [
+      '<tpl for=".">',
+      '<div data-qtip="{about}" class="thumb-wrap" id="{name}">',
+      '<div class="thumb"><img src="{iconUrl}" class="thumb-img"></div>',
+      '<span>{name}</span></div>',
+      '</tpl>',
+      '<div class="x-clear"></div>'
+      ],
+      listeners:{
+        'itemcontextmenu':function(view, record, htmlitem, index, e, options){
+          e.stopEvent();
+          var contextMenu = Ext.create("Ext.menu.Menu",{
+            items:[{
+              text:'Add Widget',
+              iconCls:'icon-add',
+              handler:function(btn){
+                record.data.onClick();
+              }
+            }]
+            });
+          contextMenu.showAt(e.xy);
+        }
       }
-    }
-  });
+    });
 
-  var widgetsPanel = new Ext.Panel({
-    id:'widgets',
-    autoDestroy:true,
-    title:'Available Widgets',
-    region:'center',
-    margins: '5 5 5 0',
-    layout:'fit',
-    items: this.widgetsDataView
-  });
+    config = Ext.apply({
+      id:'widgets',
+      autoDestroy:true,
+      title:'Widgets',
+      margins: '5 5 5 0',
+      layout:'fit',
+      items: this.widgetsDataView
+    }, config);
 
-  this.layout = new Ext.Panel({
-    layout: 'border',
-    autoDestroy:true,
-    title:'Widgets',
-    items: [widgetsPanel]
-  });
-}
+    this.callParent([config]);
+  }
+});
 
 
 
