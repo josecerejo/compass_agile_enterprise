a27c8795089aebd46e85fcaa0137b221bc23f79e
Adam H <adam@adam.local>
2012-11-04 13:44:20 -0500

    added comments

--- erp_app/public/javascripts/extjs/vtypes.js
+++ erp_app/public/javascripts/extjs/vtypes.js
@@ -2,13 +2,13 @@ Ext.onReady(function(){
     Ext.apply(Ext.form.field.VTypes, {
         //  vtype validation function
         file : function(value, field) {
-            var file_size = ErpTechSvcs.Config.max_file_size_in_mb * 1024 * 1024;
+            // var file_size = ErpTechSvcs.Config.max_file_size_in_mb * 1024 * 1024;
             var file_types = ErpTechSvcs.Config.file_upload_types.replace(/\s/g,'|').replace(/,/g,'|');
-            //var fileName = new RegExp('^.*\.('+file_types+')$','i');
             var fileName = eval('/^.*\.('+file_types+')$/i');
-            // console.log(file_size);
-            // console.log(field.inputEl.dom.files[0].size);
-            // if (field.inputEl.dom.files[0].size > file_size)) return false;
+            // file size validation only works with ExtJS's inputType == 'file'
+            // however its rendering is buggy so we leave it off for now
+            // try again when HTML5 is more widly supported
+            // if (field.inputEl.dom.files[0].size > file_size)) return false; 
             return fileName.test(value);
         },
         // vtype Text property to display error Text
