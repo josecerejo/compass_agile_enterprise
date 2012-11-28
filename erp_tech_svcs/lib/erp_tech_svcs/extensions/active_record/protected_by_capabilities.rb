e663701173d1ce9c3d412c3afbab97dc17eb6992
Adam H <adam@adam.att.net>
2012-11-28 14:47:46 -0500

    refactored scopes

--- erp_tech_svcs/lib/erp_tech_svcs/extensions/active_record/protected_by_capabilities.rb
+++ erp_tech_svcs/lib/erp_tech_svcs/extensions/active_record/protected_by_capabilities.rb
@@ -16,28 +16,47 @@ module ErpTechSvcs
             has_many :capabilities, :as => :capability_resource
 
             # get records for this model without capabilities or that are not in a list of denied roles
-            scope :with_security, lambda{|denied_roles|
-                                    joins("LEFT OUTER JOIN capable_models AS cm ON cm.capable_model_record_id = #{self.table_name}.id AND cm.capable_model_record_type = '#{self.name}'").
-                                    joins("LEFT JOIN capabilities_capable_models AS ccm ON ccm.capable_model_id = cm.id").
-                                    joins("LEFT JOIN capabilities AS c ON ccm.capability_id = c.id").
-                                    joins("LEFT JOIN secured_models AS sm ON sm.secured_record_id = c.id AND sm.secured_record_type = 'Capability'").
-                                    joins("LEFT JOIN roles_secured_models AS rsm ON rsm.secured_model_id = sm.id").
-                                    joins("LEFT JOIN roles AS r ON r.id = rsm.role_id").
-                                    where("r.id IS NULL OR r.id NOT IN (?)", denied_roles.collect{|r| r.id }).
+            scope :with_instance_security, lambda{|denied_roles|
+                                    joins("LEFT JOIN capabilities AS c ON c.capability_resource_id = #{self.table_name}.id AND c.capability_resource_type = '#{self.name}'").
+                                    where("c.id IS NULL OR c.id NOT IN (?)", denied_roles.collect{|c| c.id }).
                                     group(columns.collect{|c| "#{self.table_name}.#{c.name}" })
                                   }
 
             # get records for this model that the given user has access to
-            scope :with_user_security, lambda{|user| with_security(capability_roles - user.all_roles) }
+            scope :with_user_security, lambda{|user| with_instance_security(capabilities - user.all_capabilities) }
 				  end
 
 				end
 				
 				module SingletonMethods
-
           # class method to get all capabilities for this model
           def capabilities
             Capability.where('capability_resource_type = ?', get_superclass(self.name))
+          end
+
+          # class method to get all capabilities for this model
+          def all_capabilities
+            capabilities
+          end
+
+          # class method to get only class capabilities for this model
+          def class_capabilities
+            scope_capabilities('class')
+          end
+
+          # class method to get only query capabilities for this model
+          def query_capabilities
+            scope_capabilities('query')
+          end
+
+          # class method to get only instance capabilities for this model
+          def instance_capabilities
+            scope_capabilities('instance')
+          end
+
+          def scope_capabilities(scope_type_iid)
+            scope_type = ScopeType.find_by_internal_identifier(scope_type_iid)
+            capabilities.where(:scope_type_id => scope_type.id)
           end
 
           # collect unique roles on capabilities
@@ -45,7 +64,7 @@ module ErpTechSvcs
             capabilities.collect{|c| c.roles }.flatten.uniq
           end
 
-          # add a class level capability, capability_resource_id will be NULL
+          # add a class level capability (capability_resource_id will be NULL)
           # the purpose of this is primarily for actions like create where the record does not exist yet
           # this will allow us to assign the create capability to a User or Role so that we can ask the question "can a user create a record for this model?"
           def add_capability(capability_type_iid)
@@ -69,7 +88,6 @@ module ErpTechSvcs
           def convert_capability_type(type)
             CapabilityType.find_or_create_by_internal_identifier(type.to_s) if (type.is_a?(String) || type.is_a?(Symbol))
           end
-
 				end
 						
 				module InstanceMethods
