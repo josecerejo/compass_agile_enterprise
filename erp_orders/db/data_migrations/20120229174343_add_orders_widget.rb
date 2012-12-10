a6ec1f4b3698cdb2f2f84727de68348240ff4b17
Tom Phan <tphan@tnsolutionsinc.com>
2012-12-10 16:25:16 -0500

    updated to align with new capability features

--- erp_orders/db/data_migrations/20120229174343_add_orders_widget.rb
+++ erp_orders/db/data_migrations/20120229174343_add_orders_widget.rb
@@ -2,29 +2,29 @@ class AddOrdersWidget
   
   def self.up
     #insert data here
-    if Widget.find_by_internal_identifier('order_management').nil?
-      app = Application.find_by_internal_identifier('crm')
+   # if Widget.find_by_internal_identifier('order_management').nil?
+   #   app = Application.find_by_internal_identifier('crm')
 
-      orders = Widget.create(
-          :description => 'Order Managament',
-          :internal_identifier => 'order_management',
-          :icon => 'icon-grid',
-          :xtype => 'partyorderstab'
-        )
+   #   orders = Widget.create(
+   #       :description => 'Order Managament',
+   #       :internal_identifier => 'order_management',
+   #       :icon => 'icon-grid',
+   #       :xtype => 'partyorderstab'
+   #     )
 
-      unless app.nil?
-        app.widgets << orders
-        app.save
-      end
+   #   unless app.nil?
+   #     app.widgets << orders
+   #     app.save
+   #   end
       
-      orders.roles << SecurityRole.find_by_internal_identifier('admin')
-      orders.save      
-    end
-  end
+    #  orders.roles << SecurityRole.find_by_internal_identifier('admin')
+    #  orders.save      
+    #end
+  #end
   
   def self.down
     #remove data here
-    Widget.find_by_internal_identifier('order_management').destroy
+   # Widget.find_by_internal_identifier('order_management').destroy
   end
 
 end
