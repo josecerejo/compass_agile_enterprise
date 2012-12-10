eaaec9f5d053665624beb2f9c69733ff7e35cc9d
Tom Phan <tphan@tnsolutionsinc.com>
2012-12-10 14:36:16 -0500

    fixed default capabilities

--- erp_app/db/data_migrations/20111108183739_add_default_capabilities.rb
+++ erp_app/db/data_migrations/20111108183739_add_default_capabilities.rb
@@ -1,29 +1,42 @@
 class AddDefaultCapabilities
   
   def self.up
-    CapabilityType.create(:internal_identifier => 'edit', :description => 'Edit')
-    CapabilityType.create(:internal_identifier => 'delete', :description => 'Delete')
-    CapabilityType.create(:internal_identifier => 'create', :description => 'Create')
-    CapabilityType.create(:internal_identifier => 'view', :description => 'View')
+    #CapabilityType.create(:internal_identifier => 'edit', :description => 'Edit')
+    #CapabilityType.create(:internal_identifier => 'delete', :description => 'Delete')
+    #CapabilityType.create(:internal_identifier => 'create', :description => 'Create')
+    #CapabilityType.create(:internal_identifier => 'view', :description => 'View')
 
-    user_management_application = DesktopApplication.find_by_internal_identifier('user_management')
-    user_management_application.add_capability('create', 'User', 'admin')
-    user_management_application.add_capability('delete', 'User', 'admin')
+    admin = SecurityRole.find_by_internal_identifier('admin')
+    employee = SecurityRole.find_by_internal_identifier('employee')
 
-    notes_widget = Widget.find_by_internal_identifier('shared_notes_grid')
-    notes_widget.add_capability('create', 'Note', 'admin', 'employee')
-    notes_widget.add_capability('view', 'Note', 'admin', 'employee')
-    notes_widget.add_capability('delete', 'Note', 'admin')
+    #user_management_application = DesktopApplication.find_by_internal_identifier('user_management')
+    #user_management_application.add_capability('create', 'User', 'admin')
+    admin.add_capability('create', 'User')
+
+   # user_management_application.add_capability('delete', 'User', 'admin')
+    admin.add_capability('delete', 'User')
+
+  #  notes_widget = Widget.find_by_internal_identifier('shared_notes_grid')
+    #notes_widget.add_capability('create', 'Note', 'admin', 'employee')
+    admin.add_capability('create', 'Note')
+    employee.add_capability('create', 'Note')
+
+    #notes_widget.add_capability('view', 'Note', 'admin', 'employee')
+    admin.add_capability('view', 'Note')
+    employee.add_capability('view', 'Note')
+
+    #notes_widget.add_capability('delete', 'Note', 'admin')
+    admin.add_capability('delete', 'Note')
   end
   
   def self.down
-    CapabilityType.destroy_all("internal_identifier in (#{%w(edit,delete,create,view).map{|iid| "'#{iid}'"}.join(',')}")
+ #   CapabilityType.destroy_all("internal_identifier in (#{%w(edit,delete,create,view).map{|iid| "'#{iid}'"}.join(',')}")
 
-    user_management_application = DesktopApplication.find_by_internal_identifier('user_management')
-    user_management_application.remove_all_capabilities
+ #   user_management_application = DesktopApplication.find_by_internal_identifier('user_management')
+ #   user_management_application.remove_all_capabilities
 
-    notes_widget = Widget.find_by_internal_identifier('shared_notes_grid')
-    notes_widget.remove_all_capabilities
+ #  notes_widget = Widget.find_by_internal_identifier('shared_notes_grid')
+ #   notes_widget.remove_all_capabilities
   end
 
 end
