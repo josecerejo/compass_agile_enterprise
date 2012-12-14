98c94bc164227bf807b8bf1f50e065f5b2a9ccda
Adam H <adam@adam.olcc.lan>
2012-12-14 13:29:00 -0500

    cleaned up migrations

--- erp_app/db/data_migrations/20120229160222_add_userinfo_widget.rb
+++ erp_app/db/data_migrations/20120229160222_add_userinfo_widget.rb
@@ -2,30 +2,27 @@ class AddUserinfoWidget
   
   def self.up
     #insert data here
-#    if Widget.find_by_internal_identifier('userinfo').nil?
-#      app = Application.find_by_internal_identifier('crm')
+    if Widget.find_by_internal_identifier('userinfo').nil?
+      app = Application.find_by_internal_identifier('crm')
 
- #     user = Widget.create(
- #         :description => 'User Info',
- ##         :internal_identifier => 'userinfo',
-  #        :icon => 'icon-user',
-  #        :xtype => 'userinfo'
-  #      )
+      user = Widget.create(
+        :description => 'User Info',
+        :internal_identifier => 'userinfo',
+        :icon => 'icon-user',
+        :xtype => 'userinfo'
+      )
      
-   #   unless app.nil?
-    #    app.widgets << user
-    #    app.save
-    #  end
-      
-      #user.roles << SecurityRole.find_by_internal_identifier('admin')
-     # user.save
+      unless app.nil?
+        app.widgets << user
+        app.save
+      end
 
-    #end
+    end
   end
   
   def self.down
     #remove data here
-   # Widget.find_by_internal_identifier('userinfo').destroy
+    Widget.find_by_internal_identifier('userinfo').destroy
   end
 
 end
--- erp_invoicing/db/data_migrations/20120229174322_add_billpay_widget.rb
+++ erp_invoicing/db/data_migrations/20120229174322_add_billpay_widget.rb
@@ -2,27 +2,25 @@ class AddBillpayWidget
   
   def self.up
     #insert data here
-  #  if Widget.find_by_internal_identifier('billpay').nil?
-   #   app = Application.find_by_internal_identifier('crm')
+    if Widget.find_by_internal_identifier('billpay').nil?
+      app = Application.find_by_internal_identifier('crm')
 
-    #  billpay = Widget.create(
-     #     :description => 'Bill Pay',
-      #    :internal_identifier => 'billpay',
-      #    :icon => 'icon-grid',
-      #    :xtype => 'billpay'
-      #  )
+      billpay = Widget.create(
+        :description => 'Bill Pay',
+        :internal_identifier => 'billpay',
+        :icon => 'icon-grid',
+        :xtype => 'billpay'
+      )
 
-      #app.widgets << billpay
-      #app.save
+      app.widgets << billpay
+      app.save
 
-      #billpay.roles << SecurityRole.find_by_internal_identifier('admin')
-      #billpay.save
-  #  end
+    end
   end
   
   def self.down
     #remove data here
-    #Widget.find_by_internal_identifier('billpay').destroy
+    Widget.find_by_internal_identifier('billpay').destroy
   end
 
 end
