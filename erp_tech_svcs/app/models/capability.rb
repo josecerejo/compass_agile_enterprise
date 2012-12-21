7bdc6309888dc5c5f51553b5a9cb97b01dd1d076
Adam H <adam@adam.local>
2012-12-21 16:09:06 -0500

    capabilities tab now working

--- erp_tech_svcs/app/models/capability.rb
+++ erp_tech_svcs/app/models/capability.rb
@@ -6,4 +6,39 @@ class Capability < ActiveRecord::Base
   has_many :capability_accessors, :dependent => :destroy
 
   alias :type :capability_type
+
+
+  def roles_not
+    SecurityRole.joins("LEFT JOIN capability_accessors ON capability_accessors.capability_id = #{self.id}
+                        AND capability_accessors.capability_accessor_record_type = 'SecurityRole' 
+                        AND capability_accessors.capability_accessor_record_id = security_roles.id").
+                where("capability_accessors.id IS NULL")
+  end
+
+  def roles
+    SecurityRole.joins(:capability_accessors).where(:capability_accessors => {:capability_id => self.id })
+  end
+
+  def users_not
+    User.joins("LEFT JOIN capability_accessors ON capability_accessors.capability_id = #{self.id}
+                        AND capability_accessors.capability_accessor_record_type = 'User' 
+                        AND capability_accessors.capability_accessor_record_id = users.id").
+        where("capability_accessors.id IS NULL")
+  end
+
+  def users
+    User.joins(:capability_accessors).where(:capability_accessors => {:capability_id => self.id })
+  end
+
+  def groups_not
+    Group.joins("LEFT JOIN capability_accessors ON capability_accessors.capability_id = #{self.id}
+                        AND capability_accessors.capability_accessor_record_type = 'Group' 
+                        AND capability_accessors.capability_accessor_record_id = groups.id").
+          where("capability_accessors.id IS NULL")
+  end
+
+  def groups
+    Group.joins(:capability_accessors).where(:capability_accessors => {:capability_id => self.id })
+  end
+
 end
