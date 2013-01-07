3c29463c2f2b14261df6605c7ecdf85e124d1015
Adam H <adam@adam.local>
2013-01-07 12:24:28 -0500

    specs to test new security

--- erp_tech_svcs/app/models/group.rb
+++ erp_tech_svcs/app/models/group.rb
@@ -8,6 +8,7 @@ class Group < ActiveRecord::Base
   
   has_one :party, :as => :business_party
 
+  attr_accessible :description
   validates_uniqueness_of :description, :case_sensitive => false
 
   def self.add(description)
@@ -26,7 +27,7 @@ class Group < ActiveRecord::Base
 
   def has_role?(role)
     role = role.is_a?(SecurityRole) ? role : SecurityRole.find_by_internal_identifier(role.to_s)
-    all_roles.include?(role)
+    roles.include?(role)
   end
 
   def add_role(role)
