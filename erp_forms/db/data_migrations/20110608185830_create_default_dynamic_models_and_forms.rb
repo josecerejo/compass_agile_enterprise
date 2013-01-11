1ea56ebb22e116c1ad49e1ebb8bc033ad4a0cf1b
Adam H <adam@adam.local>
2013-01-11 16:00:36 -0500

    checks for erp_forms migrations

--- erp_forms/db/data_migrations/20110608185830_create_default_dynamic_models_and_forms.rb
+++ erp_forms/db/data_migrations/20110608185830_create_default_dynamic_models_and_forms.rb
@@ -1,30 +1,31 @@
 class CreateDefaultDynamicModelsAndForms
   
   def self.up
-    #insert data here
-    DynamicFormModel.create(:model_name => 'DynamicFormDocument')
+    DynamicFormModel.create(:model_name => 'DynamicFormDocument') if DynamicFormModel.find_by_model_name('DynamicFormDocument').nil?
+    wi = DynamicFormModel.find_or_create_by_model_name(:model_name => 'WebsiteInquiry')
 
-    fields = []
+    if DynamicForm.find_by_internal_identifier('contact_us').nil?
+      fields = []
 
-    fields << DynamicFormField.textfield({:fieldLabel => 'First Name', :name => 'first_name', :width => 250, :allowBlank => false })
-    fields << DynamicFormField.textfield({:fieldLabel => 'Last Name', :name => 'last_name', :width => 250, :allowBlank => false })
-    fields << DynamicFormField.email({:fieldLabel => 'Email', :name => 'email', :width => 250, :allowBlank => false })
-    fields << DynamicFormField.textarea({:fieldLabel => 'Message', :name => 'message', :width => 400, :height => 200, :allowBlank => false })    
+      fields << DynamicFormField.textfield({:fieldLabel => 'First Name', :name => 'first_name', :width => 250, :allowBlank => false })
+      fields << DynamicFormField.textfield({:fieldLabel => 'Last Name', :name => 'last_name', :width => 250, :allowBlank => false })
+      fields << DynamicFormField.email({:fieldLabel => 'Email', :name => 'email', :width => 250, :allowBlank => false })
+      fields << DynamicFormField.textarea({:fieldLabel => 'Message', :name => 'message', :width => 400, :height => 200, :allowBlank => false })    
 
-    d = DynamicForm.new
-    d.description = 'Contact Form'
-    d.definition = fields.to_json
-    d.model_name = 'WebsiteInquiry'
-    d.internal_identifier = 'contact_us'
-    d.default = true
-    d.dynamic_form_model = DynamicFormModel.create(:model_name => 'WebsiteInquiry')
-    d.comment = "This is the default form used by Knitkit's Contact Us Widget for Website Inquiries. NOTE: The Contact Us Widget uses Knitkit's website configuration options for email behavior & not the Dynamic Forms Widget Action setting."
-    d.msg_target = 'qtip'
-    d.save
+      d = DynamicForm.new
+      d.description = 'Contact Form'
+      d.definition = fields.to_json
+      d.model_name = 'WebsiteInquiry'
+      d.internal_identifier = 'contact_us'
+      d.default = true
+      d.dynamic_form_model = wi
+      d.comment = "This is the default form used by Knitkit's Contact Us Widget for Website Inquiries. NOTE: The Contact Us Widget uses Knitkit's website configuration options for email behavior & not the Dynamic Forms Widget Action setting."
+      d.msg_target = 'qtip'
+      d.save
+    end
   end
   
   def self.down
-    #remove data here
     DynamicFormModel.delete_all
     DynamicForm.delete_all
   end
--- erp_forms/db/data_migrations/20110828190913_create_desktop_app_dynamic_forms.rb
+++ erp_forms/db/data_migrations/20110828190913_create_desktop_app_dynamic_forms.rb
@@ -1,15 +1,17 @@
 class CreateDesktopAppDynamicForms
   def self.up
-    app = DesktopApplication.create(
-      :description => 'Dynamic Forms',
-      :icon => 'icon-document',
-      :javascript_class_name => 'Compass.ErpApp.Desktop.Applications.DynamicForms',
-      :internal_identifier => 'dynamic_forms',
-      :shortcut_id => 'dynamic_forms-win'
-    )
-    app.preference_types << PreferenceType.iid('desktop_shortcut')
-    app.preference_types << PreferenceType.iid('autoload_application')
-    app.save #probably redundant
+    if DesktopApplication.find_by_internal_identifier('dynamic_forms').nil?
+      app = DesktopApplication.create(
+        :description => 'Dynamic Forms',
+        :icon => 'icon-document',
+        :javascript_class_name => 'Compass.ErpApp.Desktop.Applications.DynamicForms',
+        :internal_identifier => 'dynamic_forms',
+        :shortcut_id => 'dynamic_forms-win'
+      )
+      app.preference_types << PreferenceType.iid('desktop_shortcut')
+      app.preference_types << PreferenceType.iid('autoload_application')
+      app.save #probably redundant
+    end
   end
 
   def self.down
