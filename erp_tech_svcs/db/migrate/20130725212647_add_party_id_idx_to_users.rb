3bbca87a3b285d31457bb60c80539c38ea42ae85
Adam H <adam@adam.local>
2013-07-26 09:14:39 -0400

    converting AddPartyIdIdxToUsers to up/down with if exists checks adding users.party_id idx to base migration

--- erp_tech_svcs/db/migrate/20130725212647_add_party_id_idx_to_users.rb
+++ erp_tech_svcs/db/migrate/20130725212647_add_party_id_idx_to_users.rb
@@ -1,5 +1,9 @@
 class AddPartyIdIdxToUsers < ActiveRecord::Migration
-  def change
-    add_index :users, :party_id
+  def up
+    add_index :users, :party_id, :name => 'users_party_id_idx' unless indexes(:users).collect {|i| i.name}.include?('users_party_id_idx')    
+  end
+
+  def down
+    remove_index :users, :party_id if indexes(:users).collect {|i| i.name}.include?('users_party_id_idx')    
   end
 end
